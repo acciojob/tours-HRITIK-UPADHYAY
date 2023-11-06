c443208e43630af2a3ba640e3ff03354442162f0
HRITIK UPADHYAY <hritikupadhyay134@gamil.com>
2023-11-06 14:04:36 +0530

    1 commit for Tours Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Tours from "./Tours";
 
 const App = () => {
     return(
       <main id="main">
+        <h1 className="title"> Tours Guide </h1>
+        <Tours />
       </main>
     )
 }
